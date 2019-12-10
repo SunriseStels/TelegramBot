a4d730dd785b9a7e829b464ea48dfecf9a86dc40
Dmitry <dima6200@gmail.com>
2019-12-10 15:54:53 +0500

    Bot add

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hologirl bot for Telegram APP
+
+##bot can use sock5 proxy
+For that you need: 
+1. Download [Tor Browser](https://www.torproject.org/download/)
+2. Open the torcc file with a text editor (Found in Tor Browser\Browser\TorBrowser\Data\Tor)
+and find ```torcc``` file
+3. Add the following lines: (configurations are described below)
+
+```
+EntryNodes {NL}
+ExitNodes {NL}
+StrictNodes 1
+SocksPort 127.0.0.1:9050
+```
+[![](https://sun9-54.userapi.com/c854028/v854028290/164932/qumrVX8WdGc.jpg)]()
---
+++ index.js
@@ -0,0 +1,28 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = "TOKEN";
+const Agent = require('socks5-https-client/lib/Agent')
+
+const smiles = ['😄','😊','😀','😋'];
+
+Array.prototype.randomElement = function(){
+    return this[Math.floor(Math.random() * this.length)];
+}
+
+const bot = new TelegramBot(token,{
+    polling: true,
+    request:{
+        agentClass: Agent,
+        agentOptions: {
+            socksHost: 'localhost',
+            socksPort: 9050
+        }
+    }
+});
+
+bot.on('message', function(msg){
+    const id = msg.chat.id;
+    if(msg.text == "!start"){
+        const rand = smiles.randomElement();
+        bot.sendMessage(id, 'work... ' + rand);
+    }
+})
